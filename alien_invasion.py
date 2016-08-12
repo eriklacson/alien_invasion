72b479aa5559a86bfc10ac9594661cf2a0355001
Erik Lacson <elacson@gmail.com>
2016-08-12 19:28:39 +0800

    added setting class in settings.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,11 +8,10 @@ def run_game():
 	# Initialize game and create a screen object.
 	pygame.init()
 
-	screen = pygame.display.set_mode((1200, 800))
-	pygame.display.set_caption("Alien Invasion")
+	ai_settings = Settings()
 
-	# Set the background color.
-	bg_color = (230, 230, 230)
+	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+	pygame.display.set_caption("Alien Invasion")
 
 	# Start the main loop for the game.
 
@@ -23,7 +22,7 @@ def run_game():
 				sys.exit()
 
 		# Redraw the screen during each pass through the loop.
-		screen.fill(bg_color)		
+		screen.fill(ai_settings.bg_color)		
 
 		# Make the most recently drawn screen visible.
 		pygame.display.flip()
