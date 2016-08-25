767114dd7396896795d0bf28ac310794bcfddf2f
Erik Lacson <elacson@gmail.com>
2016-08-25 08:16:00 +0800

    added ship controls + refactored game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -7,19 +7,23 @@ def check_events(ship):
 		if event.type == pygame.QUIT:
 			sys.exit()
 		elif event.type == pygame.KEYDOWN:
-			
-			if event.key == pygame.K_RIGHT:
-				# Move the ship to the right.
-				ship.moving_right = True
-			elif event.key == pygame.K_LEFT:
-				ship.moving_left = True
-
+			check_keydown_events(event, ship)
 		elif event.type == pygame.KEYUP:
+			check_keyup_events(event, ship)
 
-			if event.key == pygame.K_RIGHT:
-				ship.moving_right = False
-			elif event.key == pygame.K_LEFT:
-				ship.moving_left = False
+def check_keydown_events(event, ship):
+	"""Respond to keypresses."""
+	if event.key == pygame.K_RIGHT:
+		ship.moving_right = True
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = True
+
+def check_keyup_events(event, ship):
+	"""Respond to key releases."""
+	if event.key == pygame.K_RIGHT:
+		ship.moving_right = False
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = False
 
 def update_screen(ai_settings, screen, ship):
 	"""Update images on the screen and flip to the new screen."""
