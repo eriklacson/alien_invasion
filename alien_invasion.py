283c1fee54bf11ade10b0f858627ec09f2586ac6
Erik Lacson <elacson@gmail.com>
2016-09-06 09:43:15 +0800

    updated alien_invasion.py to call bullet.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,13 +24,19 @@ def run_game():
 	# Make a ship.
 	ship = Ship(ai_settings, screen)
 
+	# Make a group to store bullets in.
+	bullets = Group()
+
 	# Start the main loop for the game.
 
 	while True:
 
 		# Watch for keyboard and mouse events.
-		gf.check_events(ship)
+		gf.check_events(ai_settings, screen, ship, bullets)
+
 		# Redraw the screen during each pass through the loop.
-		gf.update_screen(ai_settings, screen, ship)
+		ship.update()
+		bullets.update()
+		gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
