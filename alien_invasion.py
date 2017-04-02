4c5df8748265ab4b1732fd0a8a37b337ceb2f841
Erik Lacson <elacson@gmail.com>
2017-04-02 12:17:35 +0800

    added function to create alien rows

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,7 +33,7 @@ def run_game():
 	bullets = Group()
 
 	# Create the fleet of aliens.
-	gf.create_fleet(ai_settings, screen, aliens)
+	gf.create_fleet(ai_settings, screen, ship, aliens)
 
 	# Start the main loop for the game.
 	while True:
