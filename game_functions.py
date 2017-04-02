225e78e8b89ee5b18477e601102733703a5d2b93
Erik Lacson <elacson@gmail.com>
2017-04-02 11:04:44 +0800

    refactored alien related functions

--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from bullets import Bullet
+from aliens import Alien
 
 def check_events(ai_settings, screen, ship, bullets):
 	"""Respond to keypresses and mouse events."""
@@ -30,19 +31,50 @@ def check_keyup_events(event, ship):
 	elif event.key == pygame.K_LEFT:
 		ship.moving_left = False
 
-def update_screen(ai_settings, screen, alien, ship, bullets):
+def update_screen(ai_settings, screen, aliens, ship, bullets):
 	"""Update images on the screen and flip to the new screen."""
 	# Redraw the screen during each pass through the loop.
 	screen.fill(ai_settings.bg_color)
 	ship.blitme()
-	alien.blitme()
+	aliens.draw(screen)
 	ship.update()
+
 	# Redraw all bullets behind ship and aliens.
 	for bullet in bullets.sprites():
 		bullet.draw_bullet()
 	# Make the most recently drawn screen visible.
 	pygame.display.flip()
 
+def get_number_aliens_x(ai_settings, alien_width):
+	"""Determine the number of aliens that fit in a row."""
+	available_space_x = ai_settings.screen_width - 2 * alien_width
+	number_aliens_x = int(available_space_x / (2 * alien_width))
+	return number_aliens_x
+
+def create_alien(ai_settings, screen, aliens, alien_number):
+	"""Create an alien and place it in the row."""
+	alien = Alien(ai_settings, screen)
+	alien_width = alien.rect.width
+	alien.x = alien_width + 2 * alien_width * alien_number
+	alien.rect.x = alien.x
+	aliens.add(alien)	
+
+
+
+
+def create_fleet(ai_settings, screen, aliens):
+	"""Create a full fleet of aliens."""
+
+	# Create an alien and find the number of aliens in a row.
+	# Spacing between each alien is equal to one alien width.
+	alien = Alien(ai_settings, screen)
+	number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+	
+	# Create the first row of aliens
+	for alien_number in range(number_aliens_x):
+		# Create an alien and place it in the row.
+		create_alien(ai_settings, screen, aliens, alien_number)
+		
 def update_bullets(bullets):
 
 	"""Update position of bullets and get rid of old bullets."""
@@ -61,5 +93,6 @@ def fire_bullet(ai_settings, screen, ship, bullets):
 	if len(bullets) < ai_settings.bullets_allowed:
 		new_bullet = Bullet(ai_settings, screen, ship)
 		bullets.add(new_bullet)
+
 
 
