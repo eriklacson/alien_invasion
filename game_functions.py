8e0c3641036f9b17741d5201648b962ab035a49e
Erik Lacson <elacson@gmail.com>
2017-07-29 11:42:44 +0800

    added dynamic speed settings

--- game_functions.py
+++ game_functions.py
@@ -22,6 +22,9 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
 	"""Start a new game when the player clicks Play."""
 	button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
 	if button_clicked and not stats.game_active:
+		# Reset the game settings.
+		ai_settings.initialize_dynamic_settings()
+
 		# Hide the mouse cursor.
 		pygame.mouse.set_visible(False)
 
@@ -132,8 +135,9 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
 	collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 	if len(aliens) == 0:
-		# Destroy existing bullets and create new fleet.
+		# Destroy existing bullets, increase speed and create new fleet.
 		bullets.empty()
+		ai_settings.increase_speed()
 		create_fleet(ai_settings, screen, ship, aliens)
 
 def fire_bullet(ai_settings, screen, ship, bullets):
